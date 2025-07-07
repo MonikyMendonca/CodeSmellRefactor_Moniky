e39148aa5244bd16d4f730d423a59435da7d109d
monikymendonca <monikymendonca18@gmail.com>
2025-07-06 21:20:30 -0300

    fix refactor code smell Data Class using Encapslate Collection refactoring technique.

--- src/main/java/org/example/studyplanner/ToDo.java
+++ src/main/java/org/example/studyplanner/ToDo.java
@@ -1,12 +1,18 @@
 package org.example.studyplanner;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class ToDo implements PlannerMaterial{
+public class ToDo implements PlannerMaterial {
     private Integer id;
     private String title;
     private String description;
     private int priority;
+
+    // Coleção encapsulada
+    private final List<String> subTasks = new ArrayList<>();
 
     public ToDo(Integer id, String title, String description, int priority) {
         this.id = id;
@@ -20,6 +26,37 @@ public class ToDo implements PlannerMaterial{
         return MessageFormat.format("[(Priority:{3}) ToDo {0}: {1}, {2}]", id, title, description, priority);
     }
 
+    // Comportamento adicional útil (ex: urgência)
+    public boolean isHighPriority() {
+        return this.priority >= 4;
+    }
+
+    public void promotePriority() {
+        if (this.priority < 5) {
+            this.priority++;
+        }
+    }
+
+    public void demotePriority() {
+        if (this.priority > 1) {
+            this.priority--;
+        }
+    }
+
+    // Subtasks (coleção encapsulada)
+    public void addSubTask(String subTask) {
+        this.subTasks.add(subTask);
+    }
+
+    public void removeSubTask(String subTask) {
+        this.subTasks.remove(subTask);
+    }
+
+    public List<String> getSubTasks() {
+        return Collections.unmodifiableList(this.subTasks);
+    }
+
+    // Getters/Setters conforme exigido pelos testes (não podem ser removidos)
     public int getId() {
         return id;
     }
